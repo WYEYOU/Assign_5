f3c24c3405f1e2ef08d857e085221d4a30a91afb
WYEYOU <65011212028@msu.ac.th>
2024-03-09 19:52:52 +0700

    movie

--- api/movie.ts
+++ api/movie.ts
@@ -1,6 +1,5 @@
 import express from "express";
 import { conn } from "../dbconnect";
-import { SeacrhResponse } from "../model/config";
 
 export const router = express.Router();
 
@@ -13,18 +12,26 @@ router.get("/search", (req, res) => {
             conn.query("select * from persons inner join creators on persons.pid = creators.pid_fk where creators.mvid_fk in (select mvid from movies where title like ?)",
                 ["%"+req.query.title+"%"],(err,ct_result,fields)=>{
                     if(err) throw err;
-                    res.json({
-                        Movie: mv_result.map((movies: any) => ({
-                            ...movies,
-                            Stars: st_result.filter(
-                              (stars: any) => stars.mvid_fk === movies.mvid
-                            ),
-                            Creators: ct_result.filter(
-                              (creators: any) => creators.mvid_fk === movies.mvid
-                            ),
-                          })),
-                    });
+
+                    // เช็คว่ามีข้อมูลหรือไม่
+                    if (mv_result.length === 0 || st_result.length === 0 || ct_result.length === 0) {
+                        // หากไม่มีข้อมูล ส่งข้อความกลับไปว่าไม่พบข้อมูล
+                        res.status(404).json({ message: "ไม่พบข้อมูล" });
+                    } else {
+                        // หากมีข้อมูล ส่งข้อมูลกลับไป
+                        res.json({
+                            Movie: mv_result.map((movies: any) => ({
+                                ...movies,
+                                Stars: st_result.filter(
+                                  (stars: any) => stars.mvid_fk === movies.mvid
+                                ),
+                                Creators: ct_result.filter(
+                                  (creators: any) => creators.mvid_fk === movies.mvid
+                                ),
+                            })),
+                        });
+                    }
             });
         });
     });
-  });
+});
